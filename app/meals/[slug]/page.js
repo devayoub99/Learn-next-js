1d0153e8512af5e60f9b9e256e34df23cab0b203
devayoub99 <admin@devayb.com>
2025-06-15 18:04:51 +0300

    NotFound, Errors, Get ONE meal

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,7 +1,35 @@
-export default function MealPage({ params }) {
+import Image from "next/image";
+import classes from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+
+export default async function MealPage({ params }) {
+  const mealData = await getMeal(params.slug);
+
+  mealData.instructions = mealData?.instructions.replace(/\n/g, "<br />");
+
   return (
     <>
-      <h2>Meal {params.slug} Page</h2>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={mealData?.image} alt={mealData?.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{mealData?.title}</h1>
+          <p className={classes.creator}>
+            By
+            <a href={`mailTo: ${mealData?.creator_email}`}>
+              {mealData?.creator}
+            </a>
+          </p>
+          <p className={classes.summary}>{mealData?.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: mealData.instructions }}
+        ></p>
+      </main>
     </>
   );
 }
