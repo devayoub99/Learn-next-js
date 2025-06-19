f414ee1ed409995aa5a04febbc0d08ee58d6e56e
devayoub99 <admin@devayb.com>
2025-06-19 18:08:53 +0300

    RevalidatePath, Add (Static, Dynamic) Meta data

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -3,6 +3,19 @@ import classes from "./page.module.css";
 import { getMeal } from "@/lib/meals";
 import { notFound } from "next/navigation";
 
+export async function generateMetadata({ params }) {
+  const mealData = await getMeal(params.slug);
+
+  if (!mealData) {
+    notFound();
+  }
+
+  return {
+    title: mealData.title,
+    description: mealData.summary,
+  };
+}
+
 export default async function MealPage({ params }) {
   const mealData = await getMeal(params.slug);
 
