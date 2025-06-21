fe1f6e31ad785f88191b4a991419d223a071113a
devayoub99 <admin@devayb.com>
2025-06-21 15:01:11 +0300

    Section 4 init

--- app/meals/not-found.js
+++
@@ -1,8 +0,0 @@
-export default function NotFoundMeal() {
-  return (
-    <main className="not-found">
-      <h1>Not found Meal!</h1>
-      <p>Unfortunately, we could not find the requested page or Meal data.</p>
-    </main>
-  );
-}
