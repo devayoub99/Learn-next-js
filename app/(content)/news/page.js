dd6ce160265391996f566df72577e4b7f18731a0
devayoub99 <admin@devayb.com>
2025-07-06 13:17:48 +0300

    Fetching data from Third party api using server-side

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,14 +1,23 @@
-import { DUMMY_NEWS } from "@/dummy-news";
 import NewsList from "@/components/news-list";
 
-export default function News() {
+export default function async News() {
+  
+  const response = await fetch("http://localhost:8080/news");
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch news");
+  }
+
+  const news = await response.json();
+  
+
   return (
     <>
       <header>
         <h2>News page</h2>
       </header>
       <main>
-        <NewsList news={DUMMY_NEWS} />
+        <NewsList news={news} />
       </main>
     </>
   );
