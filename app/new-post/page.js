c8809ee8c14b5a93ef35736292f22873755ee89a
devayoub99 <admin@devayb.com>
2025-07-10 16:48:53 +0300

    Server action in separated file & handle the Submit errors with useFormState

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,28 +1,14 @@
+"use client";
 import FormSubmit from "@/components/form-submit";
-import { storePost } from "@/lib/posts";
-import { redirect } from "next/navigation";
+import { createPost } from "@/lib/action";
+import { useFormState } from "react-dom";
 
 export default function NewPostPage() {
-    const createPost = async (formData) => {
-        "use server";
-        const title = formData.get("title");
-        const image = formData.get("image");
-        const content = formData.get("content");
-
-        await storePost({
-            userId: 1, // From DB creation.
-            imageUrl: "",
-            title,
-            content,
-        });
-
-        redirect("/feed");
-    };
-
+    const [state, action] = useFormState(createPost, {});
     return (
         <>
             <h1>Create a new post</h1>
-            <form action={createPost}>
+            <form action={action}>
                 <p className="form-control">
                     <label htmlFor="title">Title</label>
                     <input type="text" id="title" name="title" />
@@ -43,6 +29,13 @@ export default function NewPostPage() {
                 <p className="form-actions">
                     <FormSubmit />
                 </p>
+                {state.errors && (
+                    <ul className="form-errors">
+                        {state.errors.map((error) => (
+                            <li key={error}>{error}</li>
+                        ))}
+                    </ul>
+                )}
             </form>
         </>
     );
